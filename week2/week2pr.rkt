77211a4cc8cc3c60c74616a6fea4d9281ea2f5fa
Rangel <rangelplachkov1@gmail.com>
2022-10-15 22:32:07 +0300

    Tasks week 2 FP

--- week2/week2pr.rkt
+++ week2/week2pr.rkt
@@ -40,11 +40,65 @@
   (define (helper num count)
     (cond[ ( = 0 num)             count]
          [(= 0 (remainder n num))  (helper (- num 1) (+ count 1)) ]
-         [else (helper (- num 1) 0)]))
+         [else (helper (- num 1) count)]))
   (helper (- n 1) 0))
 
-(count-deviders 7)
+;(count-deviders 30)
+
+
+;(define (palindrobe n)
+ ; (define (helper result remain)
+
+
+;####################################################################  
+(define (reverse-number n)
+  (define (helper res k)
+    (if (< k 10)
+        (+ (* res 10) k)
+        (helper (+ (* res 10) (remainder k 10))
+                (quotient k 10))))
+  (helper 0 n))
+
+;(reverse-number 1234)
+;####################################################################  
+
+(define (isPalindrobe? n)
+  (if (= n (reverse-number n))
+      #t
+      #f))
+
+;(isPalindrobe? 123221)
+
+(define (count-palindromes a b)
+  (define (helper x y count)
+    (if (<= x y)
+        (if (isPalindrobe? x)
+            (helper (+ x 1) y (+ count 1))
+            (helper (+ x 1) y count))
+        count))
+  (helper a b 0))
+
+;(count-palindromes 1 9)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     
-        
-
-
