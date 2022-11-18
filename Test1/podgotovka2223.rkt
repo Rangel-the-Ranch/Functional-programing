ecb802a00d46b81cf68716a4a0608c8dda868fef
Rangel <rangelplachkov1@gmail.com>
2022-11-18 11:07:19 +0200

    preparation for test

--- Test1/podgotovka2223.rkt
+++ Test1/podgotovka2223.rkt
@@ -54,15 +54,85 @@
     ;         [else (string-append str glue (helper (+ 1 c)))]))))
 
 (define (repeater str)
-  (λ(count glue)
-    (if (= 0 couut)
-        " "
-        (λ (- count 1) glue))))
+  (λ (count glue)
+    (define (helper c)
+    (if (= c (- count 1))
+        str
+        (string-append str glue (helper (+ c 1)))))
+    (helper 0))
+  )
 
-((repeater "I love Racket") 3 " ")
+
+;((repeater "I love Racket") 3 " ")
+
+(define (sum-sum-digits a b k)
+  (define (sum-of-digits n)
+    (define (helper num sum)
+      (if (< num 10)
+          (+ sum num)
+          (helper (quotient num 10) (+ sum (remainder num 10)))))
+    (helper n 0))
+  (define (shoudAdd? num k)
+    (if (= 0 (remainder (sum-of-digits num) k))
+        #t
+        #f))
+  (define (helper c sum)
+    (cond [(> c b) sum]
+          [(shoudAdd? c k) (helper (+ c 1) (+ sum c))]
+          [else (helper (+ c 1) sum)]))
+  (helper a 0))
+
+;(sum-sum-digits 10 15 3)
+(require racket/trace)
+(define (max-ordered-sublist lst)
+  (define (find-pos maxPos currPos maxLen currLen lastNum counter xs)
+    ;(trace find-pos)
+    (cond [(> currLen maxLen)   (find-pos  currPos currPos currLen currLen lastNum counter xs)]
+          [(equal? xs null) maxPos ]
+          
+          
+          [(> lastNum (car xs)) (find-pos  maxPos  (+ counter 1) maxLen 1             0        (+ counter 1) (cdr xs)) ]
+          [else                 (find-pos  maxPos  currPos       maxLen (+ currLen 1) (car xs) (+ counter 1) (cdr xs))]))
+  
+  (define position (find-pos 0 0 0 0 0 0 lst))
+  (define (helper count xs)
+    (if (< count position)
+        (helper (+ count 1) (cdr xs))
+        xs))
+  (helper 1 lst))
+
+;(max-ordered-sublist '(1 2 3 1 2 3 4 5))
+;(max-ordered-sublist '(1 5 2 4 6 8 3 4 1)) 
+
+;;;;fix 6
+
+;(define(where list-elem list-predicates)
+ ; (define (helper result count)
+  ;  (cond [(equal? list-predicates null) '()]
+   ;       [else (map (car list-predicates) list-elem)
+          
+
+
+;(sort '(1 5 2 6 8) <)
+
+
+(define (set-union xs ys)
+  (define (remove-duplicates ls lastNumber)
+    (cond [(equal? ls null) '()]
+          [(equal? (car ls) lastNumber) (remove-duplicates (cdr ls) (car ls))]
+          [else (cons (car ls) (remove-duplicates (cdr ls) (car ls)))]))
+  (remove-duplicates (sort (append xs ys) <) #f))
+
+
+;(set-union '(1 3 5 7) '(5 7 13))
+;(set-union '(5 7 13) '(1 3 5 7))
+  
 
 
 
+
+
+;(eq? '(1) 1)
 
 
 
