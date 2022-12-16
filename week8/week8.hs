e7da62bdc59b2d9cf37163e0590d21d3953d58dc
Rangel <rangelplachkov1@gmail.com>
2022-12-16 23:09:27 +0200

    week8

--- week8/week8.hs
+++ week8/week8.hs
@@ -5,8 +5,8 @@ main = do
     --print a
     --print (f1 10 a)
     --print (myMin 5 8)
-    print (isInside 7 8 10)
-
+    --print (isInside 7 6 10)
+    --print (myFunc 3 4)
 
 a :: Int
 a = 12
@@ -23,3 +23,19 @@ isInside x a b =
     --if x > a && x < b then True else False
     x > a && x < b
 
+myFunc :: Double -> Double -> Double
+myFunc a b = (a*a + b* b)/2
+
+myFib :: Int -> Int
+myFib n = 
+    if n <= 1 then 1 else myFib (n - 2) + myFib (n - 1)
+
+
+myfib' :: Integer -> Integer
+myfib' n = helper 0 0 1
+  where
+    helper i prev cur =
+      if i == n then cur
+      else helper (i + 1) cur (prev + cur)
+
+      
