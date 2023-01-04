1ead8d9fca0f73250dcf15366d3a3ed81272d189
Rangel <rangelplachkov1@gmail.com>
2023-01-04 22:01:49 +0200

    hw3 finished

--- Homework3/hw3_1MI0800061.hs
+++ Homework3/hw3_1MI0800061.hs
@@ -4,11 +4,12 @@ import Data.Char
 
 main :: IO()
 main = do
-    print (generateAllPairs [10, 15, 25] [1, 5, 20, 30])
+    --print (concatPairBeh [1,2] [[1,2], [3,4],[4,5]] 1)
+    --print (generateAllPairs [10, 15, 25] [1, 5, 20, 30])
     --print ((generate [1,2] [5,10]))
 
-    --inputFile <- readFile "input.txt"
-    --print (countUniques inputFile) 
+    inputFile <- readFile "input.txt"
+    print (countUniques inputFile) 
 
 
 generateAllPairsByNum :: Int -> [Int] ->[[Int]]
@@ -18,25 +19,32 @@ generateAllPairsByNum n (b:bs)
         then (n : [b]) : (generateAllPairsByNum n bs)
         else (generateAllPairsByNum n bs)
 
-generateAllPairs :: [Int] -> [Int] ->[[[Int]]]
+generateAllPairs :: [Int] -> [Int] ->[[Int]]
 generateAllPairs [] _ = []
 generateAllPairs _ [] = []
-generateAllPairs (a:as) bs = (generateAllPairsByNum a bs) : (generateAllPairs as bs)
+generateAllPairs (a:as) bs = (generateAllPairsByNum a bs) ++ (generateAllPairs as bs)
+
+concatPairBeh ::[Int] -> [[Int]] -> Int -> [Int]
+concatPairBeh [] _ _ = []
+concatPairBeh res [] _ = res
+concatPairBeh main (x:xs) skips
+    = if ( (last main) < (head x) )
+        then if(skips == 0)
+            then main ++ x
+            else concatPairBeh main xs (skips-1)
+        else concatPairBeh main xs skips
+
+helper :: [[Int]] -> Int -> [[Int]] -> [[Int]]
+helper [] _ res = 
 
 
 
-helper :: [[[Int]]] ->  [Int] -> [[Int]] -> [[Int]]
-helper [] _ res = res
-helper (x:xs) curr res =
-    if( (curr == []) || (head x) > (last curr))
-        then (helper xs  (curr ++ x) (res ++ [curr] ) )
-        else (helper xs  curr res)
 
  
 generate ::[Int] -> [Int] ->[[Int]]
 generate [] _ = []
 generate _ [] = []
-generate as bs = (helper (generateAllPairs as bs) [-10] [] )
+--generate as bs = (helper (generateAllPairs as bs) [-10] [] )
 
 
 
